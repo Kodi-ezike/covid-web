351f7711a496658ba8f4080689113acac401d081
Kodi Ezike <60945912+Kodi-ezike@users.noreply.github.com>
2020-08-18 13:38:26 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+function openNav() {
+    var x = document.getElementById("links");
+    var y = document.getElementById("open");
+    var z =document.getElementById("close");
+    if(x.style.display == 'block'){
+        x.style.display = 'none';
+    }
+    else{
+        x.style.display = 'block';
+        y.style.display = 'none';
+        z.style.display = 'block';
+    }
+}
+function closeNav() {
+    var x = document.getElementById("links");
+    var y = document.getElementById("open");
+    var z =document.getElementById("close");
+    if(x.style.display == 'block'){
+        x.style.display = 'none';
+        y.style.display = 'block';
+        z.style.display = 'none';
+
+    }
+}
+
+
+
+function myFunction() {
+    var x = document.getElementById("mySelect");
+    var i = x.selectedIndex;
+    var option = document.getElementById("mySelect").appendChild(i);
+}
